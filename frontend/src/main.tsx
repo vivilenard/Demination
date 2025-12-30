a65693a54e40eef2a17f2172aab89a9f65db2277
Vivi <lenardvivien@yahoo.de>
2025-12-30 21:05:27 +0100

    basic start with to do list in material ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,55 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material'
+// import theme from './theme.ts'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+    background: {
+      default: '#ffffff',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#ffffff',
+    },
+    action: {
+      active: '#000000',
+      hover: '#000000',
+      selected: '#000000',
+      disabled: '#000000',
+    },
+  },
+  typography: { 
+    fontFamily: 'Roboto, sans-serif',
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: '1.25rem',
+      fontWeight: 400,
+    },
+    h4: {
+      fontSize: '1rem',
+      fontWeight: 300,
+    }
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
