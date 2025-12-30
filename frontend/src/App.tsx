4c1e1c2b4de930f2c8602857f6ffe576548a465b
Vivi <lenardvivien@yahoo.de>
2025-12-30 20:03:21 +0100

    new project with material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,34 @@
-// src/App.tsx
-import { ThemeProvider } from "./components/theme-provider"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AppLayout from "./components/app-layout"
-import { TodoList } from "./features/todo/todo-list"
-import { WeeklyPlanner } from "./features/workout/weekly-planner"
-import { ExerciseCatalogue } from "./features/catalogue/exercise-catalogue"
-import { AIPrompt } from "./features/prompt/ai-prompt"
-
-// Placeholder Components (Replace these later with your actual chapters)
-// const TodoPage = () => <div>Chapter 1: To-Do List</div>
-// const PlannerPage = () => <div>Chapter 2: Exercise Plan</div>
-// const CataloguePage = () => <div>Chapter 3: Exercise Catalogue</div>
-// const AIPage = () => <div>Chapter 4: AI Prompt</div>
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Routes>
-            <Route element={<AppLayout />}>
-            <Route path="/" element={<TodoList />} />
-            <Route path="/planner" element={<WeeklyPlanner />} />
-            <Route path="/catalogue" element={<ExerciseCatalogue />} />
-            <Route path="/ai" element={<AIPrompt />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
